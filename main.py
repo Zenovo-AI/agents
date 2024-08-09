f8eaa7ac5e249d9ddd663b95dad8595c081fcd8c
Shawn A Kumar <askumar1@gmail.com>
2024-08-09 08:50:51 -0600

    changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Import Dependencies
 from dotenv import load_dotenv
 import os
 from crewai import Crew, Process
@@ -5,10 +6,7 @@ from agent_definition import new_agent
 from task_definition import new_task
 import openai
 
-# Load environment variables from the .env file
-#load_dotenv()
-#OPENAI_KEY=os.getenv('OPENAI_API_KEY')
-OPENAI_API_KEY=''
+
 # Create a crew with the new agent and task
 crew = Crew(
     agents=[new_agent],
